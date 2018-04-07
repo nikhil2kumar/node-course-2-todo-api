ba12ea380a783a8500c72fe316da6f4881a6750b
nikhl2nik <kumarnikhil083@gmail.com>
2018-04-07 18:27:05 +0530

    updating  update method file

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,40 +8,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    //-------------------DeleteMany-----------------------
-    // db.collection('Todos').deleteMany({
-    //     text: 'Eat Lunch'
-    // }).then((result) => {
-    //     console.log(result);
-    // }, err => {
-    //     console.log('unable to delete datas from Todo');
-    // });
-
-    //-------------------deleteOne-----------------------
-    // db.collection('Todos').deleteOne({
-    //     text: 'Eat Lunch'
-    // }).then((result) => {
-    //     console.log(result);
-    // }, err => {
-    //     console.log('unable to delete data from Todo');
-    // });
-
-    //-------------------findOneAndUpdate-----------------------
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID('5ac89906b04b56b2809038e7')
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // },{
-    //     returnOriginal: false
-    // }).then((result) => {
-    //     console.log(result);
-    // }, err => {
-    //     console.log('unable to update data from Todo');
-    // });
-
-
     //-----Increment operator
     db.collection('Users').findOneAndUpdate({
         _id: new ObjectID('5ac86c1a1422862a30d67e36')
