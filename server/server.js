a3a20d5031af21bf4b9f98d470fc8f78eb6a1f4e
nikhl2nik <kumarnikhil083@gmail.com>
2018-04-08 15:03:27 +0530

    Add get todos id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var {mongoose} = require('./db/moongose');
 var {Todo} = require('./models/todo');
@@ -25,6 +26,17 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, error => res.status(400).send(error));
+});
+
+app.get('/todos/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send();
+    } else{
+        Todo.findById(req.params.id).then((todos) => {
+            if(todos)  res.send({todos});
+            else res.status(404).send();
+        }, error => res.status(400).send(error));
+    }
 });
 
 app.listen(3000, () => {
