e8d19de5f45ef50cf668aef72b28335315494843
nikhl2nik <kumarnikhil083@gmail.com>
2018-04-12 00:03:06 +0530

    Add delete users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -56,19 +56,29 @@ UserSchema.statics.findByToken = function (token) {
     });
 }
 
-UserSchema.statics.findByCredentials = function(email, password){
-let User = this;
-return User.findOne({email}).then(user => {
-    if(!user){
-        return Promise.reject();
-    }
-    return new Promise( (resolve, reject) => {
-        bcrypt.compare(password, user.password, (err, res) => {
-            if(res) resolve(user);
-            else reject();
+UserSchema.statics.findByCredentials = function (email, password) {
+    let User = this;
+    return User.findOne({ email }).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) resolve(user);
+                else reject();
+            })
         })
     })
-})
+}
+
+UserSchema.methods.removeToken = function (token) {
+    let user = this;
+
+    return user.update({
+        $pull:{
+            tokens:{token}
+        }
+    });
 }
 
 UserSchema.pre('save', function (next) {
